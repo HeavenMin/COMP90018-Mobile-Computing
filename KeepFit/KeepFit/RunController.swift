56465620d5c9e19396a2eb2feaccdb0de4f8f52e
Kaifu Ji <jikaifu.mel@gmail.com>
2017-10-07 21:33:38 +1100

    abandon useless fitness record

--- KeepFit/KeepFit/RunController.swift
+++ KeepFit/KeepFit/RunController.swift
@@ -301,8 +301,10 @@ class RunController: UIViewController,MKMapViewDelegate{
                 tap1.setImage(UIImage(named:"start_green"), for: .normal)
                 tap2.setImage(UIImage(named:"stop_red"), for: .normal)
                 
-                let azureOperator = AzureOperation()
-                azureOperator.insertDistanceRecord(distance: distance)
+                if distance > 50 {
+                    let azureOperator = AzureOperation()
+                    azureOperator.insertDistanceRecord(distance: distance)
+                }
                 
                 distance = 0.0
             }
